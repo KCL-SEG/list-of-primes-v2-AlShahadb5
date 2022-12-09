a88138f802ece3fd45f0b3d8c4da164070e0aad9
AlShahadb5 <k20067364@kcl.ac.uk>
2022-12-09 01:17:31 +0000

    primes2

--- primes.py
+++ primes.py
@@ -3,19 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError("has to be greater than 0!")
 
-    number = 2
-    counter = 0
+    else:
+        number = 2
+        counter = 0
 
     while counter < number_of_primes:
         prime = True
-
         for i in range(2, number):
             if number % i == 0:
                 prime = False
 
-            if prime:
-                list.append(number)
-                counter+=1
-                number+=1
+        if prime:
+            list.append(number)
+            counter+=1
+
+        number = number + 1
     return list
