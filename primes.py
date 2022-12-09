50a6868e4f3e7d447cb8b1a915725029558e4578
AlShahadb5 <k20067364@kcl.ac.uk>
2022-12-09 01:01:30 +0000

    primes

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+
+    number = 2
+    counter = 0
+
+    while counter < number_of_primes:
+        prime = True
+
+        for i in range(2, number):
+            if number % i == 0:
+                prime = False
+
+            if prime:
+                list.append(number)
+                counter+=1
+                number+=1
     return list
